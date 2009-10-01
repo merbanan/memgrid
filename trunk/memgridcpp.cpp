cf0f1a729142e7bc93b094bd5ad198020a069c41
banan@ludd.ltu.se <banan@ludd.ltu.se@8289957c-ac2d-11de-be8d-735280eae207>
2009-10-01 09:35:50 +0000

    Add c++ support and extend the dump log.

---
+++ trunk/memgridcpp.cpp
@@ -0,0 +1,40 @@
+
+#define MEMGRIDCPP_IMP
+
+#include <windows.h>
+#include <stdio.h>
+#include "memgrid.h"
+
+
+void * operator new (size_t size, const char * file, int line)
+{
+	return malloc_ld(size, file, line);
+}
+
+void * operator new[] (size_t size, const char * file, int line)
+{
+	return malloc_ld(size, file, line);
+}
+
+
+/* Dummy to avoid warnings */
+void operator delete (void * p, char const * file, int line)
+{
+	free_ld (p,file,line);
+}
+
+void operator delete[] (void * p, char const * file, int line)
+{
+	free_ld (p,file,line);
+}
+
+
+void operator delete (void * p)
+{
+	free_ld (p,NULL,NULL);
+}
+
+void operator delete[] (void * p)
+{
+	free_ld (p,NULL,NULL);
+}
